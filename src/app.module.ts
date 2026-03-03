fe3c1d6294c17bcbb9b08f33f6df9761db5a63a0
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2026-03-03 15:24:20 +0700

    Init db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { AdminModule } from './admin/admin.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CustomerModule, AdminModule],
+  imports: [
+    CustomerModule,
+    AdminModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import session from 'express-session';
+import passport from 'passport';
+import pgSession from 'connect-pg-simple';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const PgSession = pgSession(session);
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      store: new PgSession({
+        conString: 'postgres://postgres:password@localhost:5432/mydb',
+        tableName: 'session',
+      }),
+      secret: 'super-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false },
+    }),
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  app.use(passport.initialize());
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  app.use(passport.session());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
